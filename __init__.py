4ac45c62b53efb324564d99ced75bf4fa70b1a69
d-w-moore <dmoore@renci.org>
2022-03-13 11:33:09 -0400

    exclude files/dirs starting with .|_

--- __init__.py
+++ __init__.py
@@ -3,14 +3,17 @@ from celery import Celery
 import os
 
 def import_all():
-  from os.path import dirname,join,isfile,isdir
+  from os.path import dirname, join, isfile, isdir
   dir_ = (dirname(__file__))
   sub = os.listdir(dir_)
   EXT='.py'
   fil=[x[:-len(EXT)] for x in sub if isfile(join(dir_,x)) 
-                                        and x.endswith(EXT)]
-  dirs=[x for x in sub if isdir(join(dir_,x))]
-  return fil+dirs
+                                        and x.endswith(EXT)
+                                        and not x.startswith(('_','.'))]
+  dirs=[x for x in sub if isdir(join(dir_,x)) and not x.startswith(('.','_'))]
+  retval = fil + dirs
+# import pdb; pdb.set_trace()
+  return retval
 
 __all__ = import_all()
 
